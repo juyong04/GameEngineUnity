3b3110631f43b53a540e5f96dc7a28fd9866ded4
shinjuyongg <214128079+juyong04@users.noreply.github.com>
2025-09-29 12:18:28 +0900

    09_29

---
+++ mygame/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // 이동 속도 (Inspector에서 조절 가능)
+    public float moveSpeed = 5.0f;
+    
+    void Update()
+{
+    
+    // A키 - 왼쪽으로 이동
+    if (Input.GetKey(KeyCode.A))
+    {
+        transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+        Debug.Log("왼쪽으로 이동 중!");
+    }
+    
+    // D키 - 오른쪽으로 이동
+    if (Input.GetKey(KeyCode.D))
+    {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        Debug.Log("오른쪽으로 이동 중!");
+    }
+}
+}
